bad715da464f01ce7d9333c799a573ae4f4fcb0f
Frenchix <bapf62@hotmail.com>
2023-01-04 17:48:34 +0100

    init server Front and Back

---
+++ Front/BazarBizarre/src/App.vue
@@ -0,0 +1,7 @@
+<script setup>
+import Accueil from './views/Accueil.vue'
+</script>
+
+<template>
+  <Accueil />
+</template>
---
+++ Front/BazarBizarre/src/views/Accueil.vue
@@ -0,0 +1,17 @@
+<template>
+    <div class="flex flex-col items-center h-screen bg-stone-600" style="background-image:linear-gradient(rgba(10, 80, 156, 0.5), rgba(135, 80, 156, 0.9)), url('src/assets/background.jpg')">
+        <div class="text-center m-10 text-white text-xl">
+            Bienvenue sur le Bazard Bizarre Online!!
+        </div>
+        <div class="text-center m-5 text-white">
+            Pseudo : 
+            <input type="text" class="border border-black text-black p-1">
+        </div>
+    
+        <button class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded w-fit">Créer un nouveau salon</button>
+    </div>
+</template>
+
+<script setup>
+
+</script>
